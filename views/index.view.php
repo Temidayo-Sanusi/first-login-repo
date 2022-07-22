f6dee2a427557bbc211c75e698ff9cbbd1fa3625
Temidayo-Sanusi <precioussanusi45@gmail.com>
2022-07-22 17:13:06 +0100

    add css file

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,19 @@
 <?php require 'partials/head.php'; ?>
-<h1>USER LOGIN</h1>
-<h2>Welcome!</h2>
-<div>
+
+<div class="login-box">
+    <h1>USER LOGIN</h1>
+    <h2>Welcome!</h2>
     <form method="POST" action="/names">
+        <label>Username</label>
         <input name="name" type="text" placeholder="Username">
-        <input name="password" type="password" placeholder="Password">
+        <label>Password</label>
+        <input name="password" type="password" placeholder="Password" Required>
 
         <button type="Submit">Log In</button>
         <button><a href="signUp">Sign Up</a></button>
     </form>
+
+
 </div>
 
 <?php require 'partials/footer.php'; ?>
--- views/signUp.view.php
+++ views/signUp.view.php
@@ -1,17 +1,21 @@
 <?php require 'partials/head.php'; ?>
-<h1>Sign Up</h1>
-<h3>It's free and only takes a minute!</h3>
-<form>
-    <label>First Name</label>
-    <input type="text" placeholder="">
-    <label>Last Name</label>
-    <input type="text" placeholder="">
-    <label>Email</label>
-    <input type="email" placeholder="">
-    <label>Password</label>
-    <input type="password" placeholder="">
-    <label>Confirm Password</label>
-    <input type="password " placeholder="">
-    <input type="submit">
-</form>
+<div class="heading">
+    <h1>Sign Up</h1>
+    <h3>It's free and only takes a minute!</h3>
+</div>
+<div class="signup-box">
+    <form>
+        <label>First Name</label>
+        <input type="text" placeholder="">
+        <label>Last Name</label>
+        <input type="text" placeholder="">
+        <label>Email</label>
+        <input type="email" placeholder="">
+        <label>Password</label>
+        <input type="password" placeholder="">
+        <label>Confirm Password</label>
+        <input type="password " placeholder="">
+        <input type="submit">
+    </form>
+</div>
 <?php require 'partials/footer.php'; ?>
